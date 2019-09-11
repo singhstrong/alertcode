c3a0c8aa8abaa84c5b880942bd279badc5c8c105
lakers <nirbhay>
2019-09-11 08:58:03 +0530

    done

---
+++ src/restassuredlearning/keep.java
@@ -0,0 +1,20 @@
+package restassuredlearning;
+
+public class keep {
+	
+	static String i="test";
+    public static void main(String args[]){  
+	
+		
+		//static int i=9;
+
+		//int i=9;
+		
+		
+		
+		System.out.println(i);
+		
+	}
+
+}
+   
